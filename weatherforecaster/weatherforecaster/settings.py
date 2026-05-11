72b33f7701fc87ef43a09616b10f4ce036cb9b3f
Priyanka Rajpoot <priyankarajpoot991@gmail.com>
2026-05-11 19:25:12 +0530

    fixed static path

--- weatherforecaster/weatherforecaster/settings.py
+++ weatherforecaster/weatherforecaster/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
